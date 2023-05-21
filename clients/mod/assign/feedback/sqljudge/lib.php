24a09c99336ad90f3ae74c49163d644eb962d8b2
k231i <13050422+k231i@users.noreply.github.com>
2023-05-21 22:23:59 +0300

    add/update assignment instances

--- clients/mod/assign/feedback/sqljudge/lib.php
+++ clients/mod/assign/feedback/sqljudge/lib.php
@@ -22,9 +22,32 @@ function get_databases() {
     return $result;
 }
 
-// function get_max_ram_usages() {
-//     $maxsize = 1024 * 1024 * get_config('local_sqljudge', 'maxramlimit');
-//     for ($i = 1024 * 1024; $i <= $maxsize; $i *= 2)
-//         $ramusage[$i] = display_size($i);
-//     return $ramusage;
-// }
+function add_instance(stdClass $sqljAssign, $assignId) {
+    global $DB;
+
+    $returnid = null;
+    if ($assignId) {
+        $sqlj_assignment = $sqljAssign;
+        $sqlj_assignment->assignment = $assignId;
+        $returnid = $DB->insert_record('assignment_sqlj', $sqlj_assignment);
+    }
+
+    return $returnid;
+}
+
+function update_instance($sqljAssign, $assignId) {
+    global $DB;
+
+    $returnid = null;
+
+    if ($assignId) {
+        $sqlj_assignment = $sqljAssign;
+        $old_sqlj_assignment = $DB->get_record('assignment_sqlj', array('assignment' => $assignId));
+        if ($old_sqlj_assignment) {
+            $sqlj_assignment->id = $old_sqlj_assignment->id;
+            $returnid = $DB->update_record('assignment_sqlj', $sqlj_assignment);
+        }
+    }
+
+    return $returnid;
+}
