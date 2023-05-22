a0fad20421562aaa6b0879838b27af2bcd7d18ea
k231i <13050422+k231i@users.noreply.github.com>
2023-05-22 11:29:00 +0300

    check.php fix attempt 2

--- check.php
+++ check.php
@@ -2,13 +2,22 @@
 
 require_once(dirname(__FILE__) . '/../../config.php');
 
-global $OUTPUT, $PAGE;
+global $DB, $OUTPUT, $PAGE;
 
-$submissionId = optional_param('id', -1, PARAM_INT);
+$submissionId = intval(optional_param('id', -1, PARAM_INT));
 
 $url = new moodle_url('/local/sqljudge/check.php');
 $url->param('id', $submissionId);
 $PAGE->set_url($url);
+
+$assign = $DB->get_record_sql(
+    'SELECT a.course 
+    FROM {assign_submission} s
+    JOIN {assign} a
+        ON s.assignment = a.id
+    WHERE s.id = ?', array($submissionId));
+
+require_login($assign->course, false, null, false, true);
 
 $backendAddress = '127.0.0.1:5000'; //FIXME get_config('local_sqljudge', 'backendaddress');
 $backendPort = explode(':', $backendAddress)[1];
@@ -28,23 +37,20 @@ if ($resp === false) {
     $error = curl_error($curl);
     curl_close($curl);
     echo "Error: " . $error;
-    return false;
 }
 
 $respcode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
 
 curl_close($curl);
 
+echo $OUTPUT->header();
+
 if ($respcode === 200) {
-    echo $OUTPUT->header();
     echo $OUTPUT->notification('Checked', 'notifyinfo');
-    echo $OUTPUT->footer();
 } else if ($respcode === 422) {
-    echo $OUTPUT->header();
     echo $OUTPUT->notification('Checking submission failed', 'notifyerror');
-    echo $OUTPUT->footer();
 } else {
-    echo $OUTPUT->header();
     echo $OUTPUT->notification('Error sending request, try again later', 'notifyerror');
-    echo $OUTPUT->footer();
 }
+
+echo $OUTPUT->footer();
