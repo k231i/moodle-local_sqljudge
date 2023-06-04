4a95c67f3ab25362eb8fae5c4c41f80ddd6687b6
k231i <13050422+k231i@users.noreply.github.com>
2023-06-04 19:29:05 +0300

    settings attempt 2

--- settings.php
+++ settings.php
@@ -2,13 +2,13 @@
 defined('MOODLE_INTERNAL') || die;
 
 if ($hassiteconfig) {
-    $settings = new admin_settingpage('sqljudge', get_string('pluginname', 'local_sqljudge'));
+    $settings = new admin_settingpage('local_sqljudge', get_string('pluginname', 'local_sqljudge'));
 
     $settings->add(new admin_setting_configtext('local_sqljudge/backendaddress',
         get_string('backendaddress', 'local_sqljudge'),
         get_string('backendaddress_help', 'local_sqljudge'),
         '127.0.0.1:5000',
         PARAM_TEXT));
-        
+
     $ADMIN->add('localplugins', $settings);
 }
